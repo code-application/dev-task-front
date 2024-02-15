cd25aa5791b277d0b009ffd7357b2619eb303ecd
Ryoya Chiba <ryokan.jap.83.c@gmail.com>
2024-02-15 11:58:13 +0000

    test: setup sample test environment, but component test is not yet implemented

---
+++ src/libs/sample.ts
@@ -0,0 +1,7 @@
+// Jestテスト実行確認用の関数
+// 本番コードでJestを使ったテストを作成したら削除する
+const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+export default sum;
---
+++ src/test/components/sampletest.test.ts
@@ -0,0 +1,8 @@
+import { render } from "react-dom";
+
+// Delete this test when production code testing is added.
+describe("Sample Page testing", () => {
+  it("Sample UI Component test for jest setup", () => {
+    // TODO: implement UI component test for sample page(@/components/samplepage.tsx)
+  });
+});
