948100a0874013bbd5845ec77e509802ed2418a7
Ryoya Chiba <ryokan.jap.83.c@gmail.com>
2024-02-25 20:13:00 +0900

    test: implemented sample component test; but it fails

--- src/components/helloWorld.tsx
+++ src/components/helloWorld.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function HelloWorld() {
+export default async function HelloWorld() {
   const res: Response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
