0c9cbb9c4830568479ccffea4b049a782608906f
Kattsuuya <angelattack9759@gmail.com>
2024-06-09 17:42:11 +0900

    test: タスク一覧画面のユニットテストを追加
    - MSWを導入
    - その他軽微な改修
      - helloWorld.tsxに倣ってClient Componentに変更（設計観点では未検討。一旦テストを通すため）
      - タスク一覧をレンダリングするところでkeyにindexを指定したが、これだと順番が変わったときにレンダリングが壊れるので、task.idを指定して一意に決まるようにした

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Task from "@/models/task";
 import { UUID } from "crypto";
+import { useEffect, useState } from "react";
 
 /**
  * タスク一覧APIのレスポンス。
@@ -11,6 +14,11 @@ interface TaskResponseEntity {
   description: string;
 }
 
+/**
+ * タスク一覧を取得する。
+ *
+ * @returns タスク一覧
+ */
 const fetchTaskList = async (): Promise<Task[]> => {
   const response: Response = await fetch(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/tasks`,
@@ -32,16 +40,26 @@ const fetchTaskList = async (): Promise<Task[]> => {
   });
 };
 
-export default async function TaskList() {
-  const taskList: Task[] = await fetchTaskList();
+export default function TaskList() {
+  // このコンポーネントで状態管理する変数
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  // 画面の初期表示で行う処理
+  const initialize = async () => {
+    await fetchTaskList().then((tasks) => setTaskList(tasks));
+  };
+
+  useEffect(() => {
+    initialize();
+  }, [taskList]);
 
   return (
     <div>
-      {taskList.map((task, index) => {
+      {taskList.map((task) => {
         return (
           <div
             className="card w-96 bg-base-100 shadow-xl"
-            key={"task-" + index}
+            key={"task-" + task.id}
           >
             <div className="card-body">
               <p>id: {task.id}</p>
